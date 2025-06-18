507e3b0e87ab016adae1f2087fd24fcdc2bc47a5
Miraç Ziya Alev <miracziyaalev@gmail.com>
2025-06-18 12:04:23 +0300

    vercel bug fixed

--- src/app/landing/widgets/index.ts
+++ src/app/landing/widgets/index.ts
@@ -1,4 +1,3 @@
 export { default as HeroSection } from './HeroSection';
-export { default as ActionButtons } from './ActionButtons';
 export { default as AdminLoginButton } from './AdminLoginButton';
 export { default as AdminLoginModal } from './AdminLoginModal'; 
