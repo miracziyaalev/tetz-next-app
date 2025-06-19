9ebf22e41d783381da0cebc162c75888dd2365f3
Miraç Ziya Alev <miracziyaalev@gmail.com>
2025-06-19 18:27:27 +0300

    bug fixed

--- src/app/user-search/viewModel.tsx
+++ src/app/user-search/viewModel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import UserSearchView from "./view";
 
 export interface User {
